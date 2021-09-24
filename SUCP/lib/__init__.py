d37e7448b9012b05bbfaa1a163801cabd63fef14
Kosar <46893718+Seyedhosseinzadeh@users.noreply.github.com>
2021-09-24 13:33:36 +0330

    Delete __init__.py

--- SUCP/lib/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import MultiGaussianModel
-from . import PoissonFactorModel
-from . import TimeAwareMF
-from . import metrics
-from . import FriendBasedCF
-from . import LocationFriendshipBookmarkColoringAlgorithm
